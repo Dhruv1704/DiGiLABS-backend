9b64be56080339d3c22b6f853b67362ff7318167
Dhruv1701 <negidhruv1701@gmail.com>
2023-03-19 16:45:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const connectToMongo = require("./db");
-const Admin = require("./models/Admin");
 const cors = require('cors')
 
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const Admin =require("./models/Admin")
+const fs = require('fs')
+const multer = require("multer")
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -22,7 +23,43 @@ app.get('/', async (req, res) => {
     res.json(data)
 })
 
-app.use('/api/admin', require('./routes/admin'))
+// multer initialisation
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'uploads')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+const upload = multer({storage:storage})
+
+/*admin request  methods */
+
+app.post('/', upload.single('testImage'),(req,res)=>{
+    const admin = Admin.create({
+        text: req.body.text,
+        image:{
+            data: fs.readFileSync('uploads/'+ req.file.filename),
+            contentType: "image/*"
+        }
+    }).then((res)=> console.log("Logo and title changed."))
+        .catch((e)=> console.log(e,"error has occur"))
+    res.json(admin)
+})
+
+app.put('/',upload.single('testImage'),async (req, res)=>{
+    const data = await Admin.find()
+    const admin = await data[0]
+    admin.text = req.body.text
+    admin.image = {
+        data : fs.readFileSync('uploads/'+ req.file.filename),
+        contentType: "image/*"
+    }
+    await admin.save()
+    res.json(admin)
+})
 
 app.listen(port, () => {
     console.log(`App listening on https://digilabs-backend.vercel.app/`)
--- routes/admin.js
+++ routes/admin.js
@@ -1,48 +1,48 @@
-const express = require('express')
-const Admin =require("../models/Admin")
-const fs = require('fs')
-const multer = require("multer")
-
-const router = express.Router()
-
-// multer storage
-
-const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'uploads')
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
-})
-
-const upload = multer({storage:storage})
-
-router.get('/',(req, res)=>{
-    res.json("Admin page")
-})
-
-router.post('/', upload.single('testImage'),(req,res)=>{
-    const admin = Admin.create({
-        text: req.body.text,
-        image:{
-            data: fs.readFileSync('uploads/'+ req.file.filename),
-            contentType: "image/*"
-        }
-    }).then((res)=> console.log("Logo and title changed."))
-        .catch((e)=> console.log(e,"error has occur"))
-})
-
-router.put('/',upload.single('testImage'),async (req, res)=>{
-    const data = await Admin.find()
-    const admin = await data[0]
-    admin.text = req.body.text
-    admin.image = {
-        data : fs.readFileSync('uploads/'+ req.file.filename),
-        contentType: "image/*"
-    }
-    await admin.save()
-    res.json(admin)
-})
-
-module.exports = router;
+// const express = require('express')
+// const Admin =require("../models/Admin")
+// const fs = require('fs')
+// const multer = require("multer")
+//
+// const router = express.Router()
+//
+// // multer storage
+//
+// const storage = multer.diskStorage({
+//     destination:(req,file,cb)=>{
+//         cb(null,'uploads')
+//     },
+//     filename:(req,file,cb)=>{
+//         cb(null,file.originalname)
+//     }
+// })
+//
+// const upload = multer({storage:storage})
+//
+// router.get('/',(req, res)=>{
+//     res.json("Admin page")
+// })
+//
+// router.post('/', upload.single('testImage'),(req,res)=>{
+//     const admin = Admin.create({
+//         text: req.body.text,
+//         image:{
+//             data: fs.readFileSync('uploads/'+ req.file.filename),
+//             contentType: "image/*"
+//         }
+//     }).then((res)=> console.log("Logo and title changed."))
+//         .catch((e)=> console.log(e,"error has occur"))
+// })
+//
+// router.put('/',upload.single('testImage'),async (req, res)=>{
+//     const data = await Admin.find()
+//     const admin = await data[0]
+//     admin.text = req.body.text
+//     admin.image = {
+//         data : fs.readFileSync('uploads/'+ req.file.filename),
+//         contentType: "image/*"
+//     }
+//     await admin.save()
+//     res.json(admin)
+// })
+//
+// module.exports = router;
